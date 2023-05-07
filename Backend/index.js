90c9b8373cfc0e200ff02acc914d3bfe509b006d
Salis Salman <112612817+salissalmann@users.noreply.github.com>
2023-05-07 18:25:16 +0500

    Video to Youtube
    Video to YouTube

--- Backend/index.js
+++ Backend/index.js
@@ -79,7 +79,103 @@ app.get('/DisplayQuestionsMod2', async (req, res) => {
     }
 });
 
+const { google } = require('googleapis');
+const OAuth2Data = require('./Credentials.json');
 
+app.use(cors(
+    {
+        origin: 'http://localhost:3000',
+        credentials: true
+    }
+));
 
+const session = require('express-session');
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: 'SECRET',
+}
+));
+//Forntend (router-dom and axios)
 
-app.listen( 3001 , ()=> {console.log("LISTENING AT PORT: 3001")} )
+const upload1 = multer({ dest: 'doubtsolve/' });
+
+const CLIENT_ID = OAuth2Data.web.client_id;
+const CLIENT_SECRET = OAuth2Data.web.client_secret;
+const REDIRECT_URI = OAuth2Data.web.redirect_uris[0];
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+const scopes = ['https://www.googleapis.com/auth/youtube.upload'];
+
+const getAuthUrl = () => {
+  return oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: scopes,
+  });
+};
+
+app.get('/auth', (req, res) => {
+  const authUrl = getAuthUrl();
+  res.redirect(authUrl);
+});
+
+app.get('/oauth2callback', (req, res) => {
+  const authorizationCode = req.query.code;
+
+  oAuth2Client.getToken(authorizationCode, async (err, tokens) => {
+    if (err) {
+      console.error('Error retrieving access token:', err);
+      res.status(500).send('Error retrieving access token');
+      return;
+    }
+
+    oAuth2Client.setCredentials(tokens);
+
+    const youtube = google.youtube({
+      version: 'v3',
+      auth: oAuth2Client,
+    });
+
+    const videoPath = req.session.videoPath;
+    const videoTitle = req.session.videoTitle;
+
+    try {
+      const videoData = {
+        snippet: {
+          title: videoTitle,
+          description: 'Uploaded using YouTube Data API v3',
+        },
+        status: {
+          privacyStatus: 'private', // Set the privacy status as desired
+        },
+      };
+
+      const media = {
+        body: fs.createReadStream(videoPath),
+      };
+
+      const response = await youtube.videos.insert({
+        part: 'snippet,status',
+        media: media,
+        resource: videoData,
+      });
+
+      res.json({ videoId: response.data.id });
+    } catch (error) {
+      console.error('Error uploading video:', error);
+      res.status(500).send('Error uploading video');
+    }
+  });
+});
+
+app.post('/upload', upload1.single('video'), (req, res) => {
+  console.log(req.file);
+  req.session.videoTitle = req.file.originalname;
+  req.session.videoPath = `uploads/${req.file.filename}`;
+
+  res.redirect('/auth');
+});
+
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
+});
--- src/components/ExpertDashboard.jsx
+++ src/components/ExpertDashboard.jsx
@@ -1,12 +1,51 @@
-import React from 'react'
+import React , {useState} from 'react'
+import axios from 'axios'
+export default function ExpertDashboard() 
+{
+  const [Title,SetTitle] = useState()
+  const [Description  , SetDescription] = useState()
+  const [File, SetFile ] = useState()
 
-export default function ExpertsDisplay() {
-  return (
-    <>
-        <div className='Title'>Expert Solve</div>
-        <h5 id="ds-tagline">Availabe Questions</h5>
+  const handleChange = (e)=>
+  {
+    const inputValue = e.target.name
+    const value = e.target.value
+    if (inputValue==="file")
+    {
+      SetFile(e.target.files[0])
+    }
+    else if (inputValue==="title")
+    {
+      SetTitle(value)
+    }
+    else if (inputValue==="description")
+    {
+      SetDescription(value)
+    }
+  }
 
+  const Submit = (e) =>
+  {
+    e.preventDefault()
+    console.log(Title,Description,File)
     
-    </>
+  }
+
+  return (
+  <>
+    <div>ExpertDashboard</div>
+
+    <form>
+      <input onChange={handleChange} type="text" placeholder="title" name="title" />
+      <input  onChange={handleChange} type="text" placeholder="description" name="description" />
+
+      <input  onChange={handleChange} accept="video/mp4" type="file" placeholder="add video" name="file" />      
+
+      <button type="submit" onClick={Submit}>Submit</button>
+    </form>
+  </>
+
+
+
   )
 }
